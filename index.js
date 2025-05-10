2912a148f6b380020a8c243740b5b9428b3ce7ca
SzRegina <regina.szeprethy@gmail.com>
2025-05-10 09:20:51 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const components = {
+    button: 'search-button',
+    input: 'search-input',
+    container: 'pokemon-container'
+}
+
+document.getElementById(components.button).addEventListener('click',()=>{
+    alert("Keresünk")
+})
+
