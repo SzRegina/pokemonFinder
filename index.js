5f0c1638320acb8f5fe340bfb20d3fb60dd1d968
SzRegina <regina.szeprethy@gmail.com>
2025-05-10 10:26:22 +0200

    refak, gomd disable és del after last

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const components = {
-    button: 'search-button',
-    input: 'search-input',
-    container: 'pokemon-container'
+const $ = id => document.getElementById(id);
+
+const OPTIONS = {
+    api: 'https://pokeapi.co/api/v2/pokemon/'
+};
+
+const getPokemonByName = async name =>
+    (await fetch(`${OPTIONS.api}${name}`)).json();
+
+function setButtonState() {
+    const inputValue = $('search-input').value.trim();
+    $('search-button').disabled = inputValue === '';
 }
 
-document.getElementById(components.button).addEventListener('click',()=>{
-    alert("Keresünk")
-})
+$('search-button').addEventListener('click', async () => {
+    try {
+        $('pokemon-container').innerHTML = ''; // törli az előző képet
+        const data = await getPokemonByName($('search-input').value.trim());
+        const img = document.createElement('img');
+        img.src = data.sprites.front_default;
+        $('pokemon-container').append(img);
+    } catch (e) {
+        console.error("Hiba történt a Pokémon lekérdezés során:", e);
+    }
+});
 
+$('search-input').addEventListener('input', setButtonState);
+
+setButtonState();
